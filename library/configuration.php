3743568f6af003e5920c2fa8e3f4c0907b558caa
Ajit Gaddam <ajit@root777.com>
2014-09-07 22:07:34 -0700

    Create configuration.php

---
+++ library/configuration.php
@@ -0,0 +1,12 @@
+<?php
+/* This section contains the database connection parameters 
+* for the server. The server will try to connect to the database 
+* $dbname at the host $host with the username and password $dbuser 
+* and $dbpass as specified in this file.
+*/
+
+$host="localhost";
+$dbname="ephp";
+$dbuser="ephp";
+$dbpass="ephp";
+?>
